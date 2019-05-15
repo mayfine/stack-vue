1cc3bd6b0f454ee03bd5a1f4746e901783e4d141
dev-meifan <727156131@qq.com>
2019-05-15 23:08:05 +0800

    feature: 添加带loading异步高阶组件

---
+++ my-app-demo/src/demo/HOC/hoc.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>高阶组件示例</h3>
+        <hoc-component name="may" :age="age" @check="check">
+            <div slot="title">this is slot title...</div>
+            <div slot="content">this is slot content...</div>
+        </hoc-component>
+    </div>
+</template>
+
+<script>
+import HOC from './index.js';
+let HocComponent = HOC(import('./test-component'));
+
+export default {
+    components: {
+        HocComponent
+    },
+
+    data () {
+        return {
+            age: 18
+        }
+    },
+
+    methods: {
+        check () {
+            console.log('click...')
+        }
+    }
+}
+</script>
+
